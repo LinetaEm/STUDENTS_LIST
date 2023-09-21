ed8ef59d077e2fcc7234d798dc3e3715e96950f2
LinetaEm <143501281+LinetaEm@users.noreply.github.com>
2023-09-21 18:55:59 +0300

    students_list

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("form");
+    const studentsList = document.querySelector("#students-list");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+
+        const name = document.querySelector("#studentName").value;
+        const age = document.querySelector("#age").value;
+        const telephone = document.querySelector("#telephone").value;
+        const email = document.querySelector("#email").value;
+        const skills = document.querySelector("#skills").value;
+        const group = document.querySelector('input[name="group"]:checked').value;
+        const languages = Array.from(document.querySelectorAll('input[name="languages"]:checked')).map(input => input.value);
+
+
+        const studentItem = document.createElement("div");
+        studentItem.className = "student-item";
+        studentItem.innerHTML = `
+        <div>
+        <h2>${studentName.value} ${surname.value}</h2>
+            <p>Age: ${age}</p>
+            <p>Tel: ${telephone}</p>
+            <p>Email: ${email}</p>
+            <p>IT skills: ${skills}/10</p>
+            <p>Group: ${group}</p>
+            <p>Languages of interest: ${languages.join(", ")}</p>
+        </div>`;
+        studentsList.insertBefore(studentItem, studentsList.firstChild);
+        form.reset();
+
+        const value = document.querySelector("#value");
+        const input = document.querySelector("#studentName"); 
+
+        value.textContent = input.value;
+
+        input.addEventListener("input", (event) => {
+            value.textContent = event.target.value;
+        });
+
+    });
+});
