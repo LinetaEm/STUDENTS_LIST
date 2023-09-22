4c3e507dbb0142d9d8fbf63de0307bf1cdba845a
LinetaEm <143501281+LinetaEm@users.noreply.github.com>
2023-09-22 08:45:36 +0300

    09-22

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
+const form = document.querySelector('form');
+const studentsList = document.getElementById('students-list');
 
-    const form = document.querySelector("form");
-    const studentsList = document.querySelector("#students-list");
+function handleSubmit(event) {
+    event.preventDefault(); 
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); 
+    const name = document.getElementById('studentName').value;
+    const surname = document.getElementById('surname').value;
+    const age = document.getElementById('age').value;
+    const telephone = document.getElementById('telephone').value;
+    const email = document.getElementById('email').value;
+    const skills = document.getElementById('skills').value;
+    const group = document.querySelector('input[name="group"]:checked').value;
+    const languages = Array.from(document.querySelectorAll('input[name="languages"]:checked')).map(input => input.value);
 
-        const studentName = document.querySelector("#studentName").value;
-        const age = document.querySelector("#age").value;
-        const telephone = document.querySelector("#telephone").value;
-        const email = document.querySelector("#email").value;
-        const skills = document.querySelector("#skills").value;
-        const group = document.querySelector('input[name="group"]:checked').value;
-        const languages = Array.from(document.querySelectorAll('input[name="languages"]:checked')).map(input => input.value);
+    const studentItem = document.createElement('div');
+    studentItem.classList.add('student-item');
 
+    studentItem.innerHTML = `
+        <h2>${name} ${surname}</h2>
+        <p>Age: ${age}</p>
+        <p>Telephone: ${telephone}</p>
+        <p>Email: ${email}</p>
+        <p>Skills: ${skills}</p>
+        <p>Group: ${group}</p>
+        <p>Languages: ${languages.join(', ')}</p>
+    `;
 
-        const studentItem = document.createElement("div");
-        studentItem.className = "student-item";
-        studentItem.innerHTML = `
-        <div>
-        <h2>${studentName.value} ${surname.value}</h2>
-            <p>Age: ${age}</p>
-            <p>Tel: ${telephone}</p>
-            <p>Email: ${email}</p>
-            <p>IT skills: ${skills}/10</p>
-            <p>Group: ${group}</p>
-            <p>Languages of interest: ${languages.join(", ")}</p>
-        </div>`;
-        studentsList.insertBefore(studentItem, studentsList.firstChild);
-        form.reset();
+    studentsList.insertBefore(studentItem, studentsList.firstChild);
 
-    });
-});
+    form.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
