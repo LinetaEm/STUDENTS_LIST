9b3dbb210baf0856b7e1167e4929eaea76cd18a8
LinetaEm <143501281+LinetaEm@users.noreply.github.com>
2023-09-22 13:46:18 +0300

    changes5

--- script.js
+++ script.js
@@ -1,118 +1,86 @@
-const form = document.querySelector('form');
-const studentsList = document.getElementById('students-list');
-const skillsValue = document.getElementById('skills-value');
+    const form = document.querySelector('form');
+    const studentsList = document.getElementById('students-list');
+    const skillsValue = document.getElementById('skills-value');
 
-function handleSubmit(event) {
-event.preventDefault();
+    function handleSubmit(event) {
+    event.preventDefault();
 
-const name = document.getElementById('studentName').value;
-const surname = document.getElementById('surname').value;
-const age = document.getElementById('age').value;
-const telephone = document.getElementById('telephone').value;
-const email = document.getElementById('email').value;
-const skills = document.getElementById('skills').value;
-const group = document.querySelector('input[name="group"]:checked').value;
-const languages = Array.from(document.querySelectorAll('input[name="languages"]:checked')).map(input => input.value);
+    const name = document.getElementById('studentName').value;
+    const surname = document.getElementById('surname').value;
+    const age = document.getElementById('age').value;
+    const telephone = document.getElementById('telephone').value;
+    const email = document.getElementById('email').value;
+    const skills = document.getElementById('skills').value;
+    const group = document.querySelector('input[name="group"]:checked').value;
+    const languages = Array.from(document.querySelectorAll('input[name="languages"]:checked')).map(input => input.value);
 
-let asteriskEmail = email.replace(/.*/g, '*');
-let asteriskTelephone = telephone.replace(/.*/g, '*');
+    let asteriskEmail = email.replace(/.*/g, '*');
+    let asteriskTelephone = telephone.replace(/.*/g, '*');
 
-let personalDataDiv = document.createElement('div');
-personalDataDiv.classList.add('personal-data');
-personalDataDiv.style.display = 'none';
+    let personalDataDiv = document.createElement('div');
+    personalDataDiv.classList.add('personal-data');
+    personalDataDiv.style.display = 'none';
 
-let studentItem = document.createElement('div');
-studentItem.classList.add('student-item');
+    let studentItem = document.createElement('div');
+    studentItem.classList.add('student-item');
 
-let showButton = document.createElement('button');
-showButton.textContent = 'Show personal data';
+    let showButton = document.createElement('button');
+    showButton.textContent = 'Show personal data';
 
-studentItem.innerHTML = `
-<h2>${name} ${surname}</h2>
-<p>Age: ${age}</p>
-<p>Telephone: ${telephone}</p>
-<p>Email: ${email}</p>
-<p>Skills: ${skills}</p>
-<p>Group: ${group}</p>
-<p>Languages: ${languages.join(', ')}</p>
-`;
+    studentItem.innerHTML = `
+    <h2>${name} ${surname}</h2>
+    <p>Age: ${age}</p>
+    <p>Telephone: ${telephone}</p>
+    <p>Email: ${email}</p>
+    <p>Skills: ${skills}</p>
+    <p>Group: ${group}</p>
+    <p>Languages: ${languages.join(', ')}</p>
+    `;
 
-studentItem.appendChild(showButton);
-studentItem.appendChild(personalDataDiv);
+    studentItem.appendChild(showButton);
+    studentItem.appendChild(personalDataDiv);
 
-studentsList.insertBefore(studentItem, studentsList.firstChild);
-form.reset();
+    studentsList.insertBefore(studentItem, studentsList.firstChild);
+    form.reset();
 
-studentCreatedNow(name, surname);
+    studentCreatedNow(name, surname);
 
-let emailDisplay = document.createElement('p');
-let phoneDisplay = document.createElement('p');
+    let emailDisplay = document.createElement('p');
+    let phoneDisplay = document.createElement('p');
 
-showButton.addEventListener('click', () => {
-if (showButton.textContent === 'Show personal data') {
-personalDataDiv.style.display = 'block';
-showButton.textContent = 'Hide personal data';
-emailDisplay.textContent = email;
-phoneDisplay.textContent = telephone;
-} else {
-personalDataDiv.style.display = 'none';
-showButton.textContent = 'Show personal data';
-emailDisplay.textContent = asteriskEmail;
-phoneDisplay.textContent = asteriskTelephone;
-}
-});
-
-}
-
-function studentCreatedNow(firstName, lastName) {
-let messageSpan = document.createElement('span');
-messageSpan.textContent = `Student created (${firstName} ${lastName})`;
-messageSpan.classList.add('student-created-message');
-
-studentsList.insertBefore(messageSpan, studentsList.firstChild);
-
-setTimeout(() => {
-studentsList.removeChild(messageSpan); 
-}, 5000);
-}
-
-
-form.addEventListener('submit', handleSubmit);
-
-document.getElementById('skills').addEventListener('input', function () {
-skillsValue.textContent = `Range: ${this.value}`;
-});
-
-
-function deleteStudent(studentItem, name, surname) {
-    const confirmation = confirm(`Ar tikrai norite ištrinti studentą ${name} ${surname}?`);
-    if (confirmation) {
-    studentsList.removeChild(studentItem);
-    showDeleteMessage(name, surname);
+    showButton.addEventListener('click', () => {
+    if (showButton.textContent === 'Show personal data') {
+    personalDataDiv.style.display = 'block';
+    showButton.textContent = 'Hide personal data';
+    emailDisplay.textContent = email;
+    phoneDisplay.textContent = telephone;
+    } else {
+    personalDataDiv.style.display = 'none';
+    showButton.textContent = 'Show personal data';
+    emailDisplay.textContent = asteriskEmail;
+    phoneDisplay.textContent = asteriskTelephone;
     }
-}
+    });
 
-function showDeleteMessage(name, surname) {
-    const deleteMessageSpan = document.createElement('span');
-    deleteMessageSpan.textContent = `Studentas (${name} ${surname}) sėkmingai ištrintas.`;
-    deleteMessageSpan.classList.add('student-deleted-message');
+    }
 
-    studentsList.insertBefore(deleteMessageSpan, studentsList.firstChild);
+    function studentCreatedNow(firstName, lastName) {
+    let messageSpan = document.createElement('span');
+    messageSpan.textContent = `Student created (${firstName} ${lastName})`;
+    messageSpan.classList.add('student-created-message');
+
+    studentsList.insertBefore(messageSpan, studentsList.firstChild);
 
     setTimeout(() => {
-    studentsList.removeChild(deleteMessageSpan);
+    studentsList.removeChild(messageSpan); 
     }, 5000);
-}
+    }
 
-const studentItems = document.querySelectorAll('.student-item');
-studentItems.forEach((studentItem) => {
-    const nameElement = studentItem.querySelector('h2');
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Ištrinti studentą';
-    studentItem.appendChild(deleteButton);
 
-    deleteButton.addEventListener('click', () => {
-    const [name, surname] = nameElement.textContent.split(' ');
-    deleteStudent(studentItem, name, surname);
+    form.addEventListener('submit', handleSubmit);
+
+    document.getElementById('skills').addEventListener('input', function () {
+    skillsValue.textContent = `Range: ${this.value}`;
     });
-});
+
+
